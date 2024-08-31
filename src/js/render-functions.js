0eb624983a800c6eb655e549bd0afc184d66c842
Olga Oliynik <poddubnaolga@gmail.com>
2024-08-31 23:29:53 +0300

    some

--- src/js/render-functions.js
+++ src/js/render-functions.js
@@ -1,6 +1,8 @@
 import iziToast from "izitoast";
 import "izitoast/dist/css/iziToast.min.css";
 
+
+
 import SimpleLightbox from "simplelightbox";
 import "simplelightbox/dist/simple-lightbox.min.css";
 
